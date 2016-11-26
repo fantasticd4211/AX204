f446fa8b7518cd9a39b51a4749324c2fde24ac84
Daanyal Ebrahim <fantasticd4211@gmail.com>
2016-11-26 10:15:16 +0800

    PazerLazer

--- PhaserGames/app.js
+++ PhaserGames/app.js
@@ -73,7 +73,7 @@ function update(){
 			player.body.velocity.x = -150;
 			player.animations.play('right');
 	} else {
-		player.animation.stop();
+		player.animations.stop();
 		player.frame = 4;
 	}
 	//allow players to jump
